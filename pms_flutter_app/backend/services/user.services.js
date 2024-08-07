c0837af7d31fd0fc074070e462d5441f8eb42d76
parvezhasan2001 <parvezh919@gmail.com>
2024-08-08 00:44:27 +0600

    backend added

---
+++ pms_flutter_app/backend/services/user.services.js
@@ -0,0 +1,34 @@
+const UserModel = require('../model/user.model')
+const jwt = require("jsonwebtoken");
+
+class UserService {
+     static async registerUser(userSchema) {
+          try {
+               const createUser = new UserModel(userSchema);
+               return await createUser.save();
+          } catch (err) {
+               throw err;
+          }
+     }
+     static async getUserByEmail(email) {
+          try {
+               return await UserModel.findOne({ email });
+          } catch (err) {
+               console.log(err);
+          }
+     }
+
+     static async checkUser(email) {
+          try {
+               return await UserModel.findOne({ email });
+          } catch (error) {
+               throw error;
+          }
+     }
+
+     static async generateAccessToken(tokenData, JWTSecret_Key, JWT_EXPIRE) {
+          return jwt.sign(tokenData, JWTSecret_Key, { expiresIn: JWT_EXPIRE });
+     }
+}
+
+module.exports = UserService;
---
+++ pms_flutter_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'signup.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const LoginPage(),
+      routes: {
+        '/signup': (context) => const SignUpPage(),
+      },
+    );
+  }
+}
