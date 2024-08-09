f2825de6fe7c89d143e8a0b7a5ce74350dcc9aec
parvezhasan2001 <parvezh919@gmail.com>
2024-08-09 18:31:50 +0600

    Backend Updated

--- android/app/src/main/kotlin/com/example/passport_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.passport_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ backend/model/user.model.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const db = require('../config/db');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+     fname:
+     {
+          type: String,
+          required: true
+     },
+     lname:
+     {
+          type: String,
+          required: true
+     },
+     email:
+     {
+          type: String,
+          required: true, unique: true
+     },
+     dob:
+     {
+          type: Date,
+          default: () => new Date().toISOString().split('T')[0],
+          required: true
+     },
+     phone:
+     {
+          type: Number,
+          required: true
+     },
+     nid:
+     {
+          type: String,
+          required: true,
+     },
+     password:
+     {
+          type: String,
+          required: true
+     }
+});
+
+userSchema.pre('save', async function () {
+     try {
+          var user = this;
+          const salt = await (bcrypt.genSalt(10));
+          const hashpass = await bcrypt.hash(user.password, salt);
+
+          user.password = hashpass;
+     } catch (error) {
+          throw error;
+     }
+});
+
+//used while signIn decrypt
+userSchema.methods.comparePassword = async function (candidatePassword) {
+     try {
+          console.log('----------------no password', this.password);
+          // @ts-ignore
+          const isMatch = await bcrypt.compare(candidatePassword, this.password);
+          return isMatch;
+     } catch (error) {
+          throw error;
+     }
+};
+const UserModel = db.model('User', userSchema);
+module.exports = UserModel;
