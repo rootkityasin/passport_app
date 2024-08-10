0379a16545a9f2cd7bc974e55b049bc6e8ea6d02
rootkityasin <107543212+rootkityasin@users.noreply.github.com>
2024-08-11 00:51:13 +0600

    backend fixed

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
+
 const app = require('./app');
-const db = require('./config/db')
-const userModel = require('./model/user.model')
+const db = require('./config/db');
+const userModel = require('./model/user.model');
 const port = 8000;
 
 app.get('/', (req, res) => {
-     res.send("Hello World")
+     res.send("Hello World");
 });
 
-app.listen(port, () => {
-     console.log(`Server Listening on Port http://192.168.0.110:${port}`);
+app.listen(port, 'localhost', () => {
+     console.log(`Server Listening on Port http://localhost:${port}`);
 });
--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,4 @@
-const url = 'http://192.168.0.110:8000/';
+
+const url = 'http://localhost:8000/';
 const registration = "${url}api/users/register";
 const login = "${url}api/users/login";
