37c39a568aa0e60d47af96233eb81a311c0fa635
rootkityasin <107543212+rootkityasin@users.noreply.github.com>
2024-08-30 15:29:12 +0600

    Update police_dashboard.dart

--- lib/police/police_dashboard.dart
+++ lib/police/police_dashboard.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
+ import 'dart:ui'; 
 
 class PoliceDashboard extends StatefulWidget {
-  final token;
-  const PoliceDashboard({@required this.token, super.key});
+  final String token;
+  const PoliceDashboard({required this.token, super.key});
 
   @override
   PoliceDashboardState createState() => PoliceDashboardState();
 }
-
 class PoliceDashboardState extends State<PoliceDashboard> {
   late String pid;
+  int _selectedIndex = 0;
+
   @override
   void initState() {
     super.initState();
     Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(widget.token);
-
     pid = jwtDecodedToken['pid'];
   }
 
-  int _selectedIndex = 0;
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -195,292 +194,90 @@ class PoliceDashboardState extends State<PoliceDashboard> {
     );
   }
 
-  Widget _buildAnimatedCard(String title, String subtitle, IconData icon,
-      Color iconColor, VoidCallback onPressed,
-      {Color textColor = Colors.white}) {
-    return Card(
-      color: const Color(0xFFD9D9D9), // Set the card color
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Icon(icon, color: iconColor, size: 35), // Icon size
-                const SizedBox(width: 20), // Space between icon and title
-                Expanded(
-                  child: Text(
-                    title,
-                    style: const TextStyle(
-                        fontSize: 17, fontWeight: FontWeight.bold),
-                    softWrap: true,
-                  ),
+Widget _buildAnimatedCard(String title, String subtitle, IconData icon,
+    Color iconColor, VoidCallback onPressed,
+    {Color textColor = Colors.white}) {
+  bool isHovered = false;
+
+  return StatefulBuilder(
+    builder: (BuildContext context, StateSetter setState) {
+      return MouseRegion(
+        onEnter: (_) {
+          setState(() {
+            isHovered = true;
+          });
+        },
+        onExit: (_) {
+          setState(() {
+            isHovered = false;
+          });
+        },
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 700),
+          transform: isHovered ? Matrix4.identity().scaled(1.04) : Matrix4.identity(),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.3),
+              borderRadius: BorderRadius.circular(16.0),
+              border: Border.all(
+                width: 1.5,
+                color: Colors.white.withOpacity(0.3),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: iconColor.withOpacity(isHovered ? 0.8 : 0.5),
+                  blurRadius: isHovered ? 20.0 : 0.5,
+                  spreadRadius: isHovered ? 5.0 : 0.5,
                 ),
               ],
             ),
-            const SizedBox(height: 8),
-            const Spacer(), // Pushes the button to the bottom
-            Center(
-              child: ElevatedButton(
-                onPressed: onPressed, // Handle button press
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF186343), // Background color
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(12.0), // Rounded corners
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16.0, horizontal: 24.0), // Increase padding
-                  textStyle:
-                      const TextStyle(fontSize: 16), // Increase font size
-                ),
-                child: Text(
-                  subtitle,
-                  style: TextStyle(color: textColor), // Custom font color
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class PoliceDashboardPage extends StatefulWidget {
-  const PoliceDashboardPage({super.key});
-
-  @override
-  PoliceDashboardPageState createState() => PoliceDashboardPageState();
-}
-
-class PoliceDashboardPageState extends State<PoliceDashboardPage> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Police Dashboard'),
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {
-            // Handle menu button press
-          },
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.person),
-            onPressed: () {
-              // Handle logout
-            },
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                prefixIcon: const Icon(
-                  Icons.search,
-                  color: Colors.black54,
-                ),
-                hintText: 'Search for something',
-                hintStyle: const TextStyle(
-                  color: Colors.black54,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  borderSide: BorderSide.none,
-                ),
-                filled: true,
-                fillColor:
-                    Colors.grey[200], // Background color of the search bar
-              ),
-            ),
-            const SizedBox(height: 50), // Increased height to lower the cards
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16.0,
-                mainAxisSpacing: 16.0,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  _buildAnimatedCard(
-                    "Verify Applications", // Card title
-                    "Check and Verify", // Card subtitle
-                    Icons.verified_user, // Card icon
-                    Colors.blue, // Icon color
-                    () {
-                      // Handle card press
-                    },
+                  Row(
+                    children: [
+                      Icon(icon, color: iconColor, size: 35),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: Text(
+                          title,
+                          style: const TextStyle(
+                              fontSize: 17, fontWeight: FontWeight.bold),
+                          softWrap: true,
+                        ),
+                      ),
+                    ],
                   ),
-                  _buildAnimatedCard(
-                    "View Reports", // Card title
-                    "Check Reports", // Card subtitle
-                    Icons.report, // Card icon
-                    Colors.red, // Icon color
-                    () {
-                      // Handle card press
-                    },
-                  ),
-                  _buildAnimatedCard(
-                    "Settings", // Card title
-                    "Configure System", // Card subtitle
-                    Icons.settings, // Card icon
-                    Colors.green, // Icon color
-                    () {
-                      // Handle card press
-                    },
-                  ),
-                  _buildAnimatedCard(
-                    "Notifications", // Card title
-                    "View Alerts", // Card subtitle
-                    Icons.notifications, // Card icon
-                    Colors.orange, // Icon color
-                    () {
-                      // Handle card press
-                    },
+                  const SizedBox(height: 8),
+                  const Spacer(),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: onPressed,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF186343),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 16.0, horizontal: 24.0),
+                        textStyle: const TextStyle(fontSize: 16),
+                      ),
+                      child: Text(
+                        subtitle,
+                        style: TextStyle(color: textColor),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-          ],
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed, // To maintain colors for each item
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: _selectedIndex == 0
-                ? Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        width: 40,
-                        height: 40,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.green, // Color for selected item
-                        ),
-                      ),
-                      const Icon(Icons.home,
-                          color: Colors.black), // Icon for home
-                    ],
-                  )
-                : const Icon(Icons.home, color: Colors.black), // Icon for home
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: _selectedIndex == 1
-                ? Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        width: 40,
-                        height: 40,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.green, // Color for selected item
-                        ),
-                      ),
-                      const Icon(Icons.search,
-                          color: Colors.black), // Icon for search
-                    ],
-                  )
-                : const Icon(Icons.search,
-                    color: Colors.black), // Icon for search
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: _selectedIndex == 2
-                ? Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        width: 40,
-                        height: 40,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.green, // Color for selected item
-                        ),
-                      ),
-                      const Icon(Icons.account_circle,
-                          color: Colors.black), // Icon for account
-                    ],
-                  )
-                : const Icon(Icons.account_circle,
-                    color: Colors.black), // Icon for account
-            label: '',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        selectedItemColor: Colors.green, // Color for the selected item
-        unselectedItemColor: Colors.grey, // Color for unselected items
-        showSelectedLabels: false, // Hides labels
-        showUnselectedLabels: false, // Hides labels
-      ),
-    );
-  }
-
-  Widget _buildAnimatedCard(String title, String subtitle, IconData icon,
-      Color iconColor, VoidCallback onPressed,
-      {Color textColor = Colors.white}) {
-    return Card(
-      color: const Color(0xFFD9D9D9), // Set the card color
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Icon(icon, color: iconColor, size: 35), // Icon size
-                const SizedBox(width: 20), // Space between icon and title
-                Expanded(
-                  child: Text(
-                    title,
-                    style: const TextStyle(
-                        fontSize: 17, fontWeight: FontWeight.bold),
-                    softWrap: true,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            const Spacer(), // Pushes the button to the bottom
-            Center(
-              child: ElevatedButton(
-                onPressed: onPressed, // Handle button press
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF186343), // Background color
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(12.0), // Rounded corners
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16.0, horizontal: 24.0), // Increase padding
-                  textStyle:
-                      const TextStyle(fontSize: 16), // Increase font size
-                ),
-                child: Text(
-                  subtitle,
-                  style: TextStyle(color: textColor), // Custom font color
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+      );
+    },
+  );
 }
+}
+ 
